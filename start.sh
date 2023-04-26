7b44625df64008779cee10e27fb3ef491f2f118d
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-04-26 23:01:11 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 git clone $REPO_URL ok && cd ok
 pip3 install poetry -y
-$START_CMD
+apt-get -qq update && apt-get -qq upgrade -y
+apt-get -qq install -y --no-install-recommends \
+
+    curl \
+
+    git \
+
+    gnupg2
