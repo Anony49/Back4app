049fc2485e777ff9bacd7e5e42a9b70a203a90bb
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-04-26 23:18:46 +0600

    lol

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 git clone $REPO_URL ok && cd ok
 pip3 install poetry -y
 apt-get -qq update && apt-get -qq upgrade -y
+#ok
 apt-get -qq install -y --no-install-recommends \
 pip install --upgrade pip
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py
