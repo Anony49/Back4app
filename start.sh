0353a6c0490d15c4c54fbde4564aff849147d771
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-05-07 12:46:21 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && apt-get -qq update && apt-get -qq upgrade -y && apt-get -qq install -y --no-install-recommends curl git gnupg2 && pip3 install poetry -y && pip install --upgrade pip && curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python - && poetry config virtualenvs.create false && poetry install --no-root --only main -E uvloop
+git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
 $START_CMD
