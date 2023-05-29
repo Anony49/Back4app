c2a828f709545cb5774a45f419429ef181a87cdf
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-05-29 17:01:51 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
+git clone $REPO_URL ok && cd ok && pip install --upgrade pip && pip install poetry && poetry install --no-root --only main -E uvloop 
 $START_CMD
